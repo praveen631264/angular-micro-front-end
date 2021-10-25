8ce4dd0a3effcd7395fd0b2ecabea6fce161e639
Devops Admin <azure_dealsplus@dealsplus.io>
2021-10-25 19:22:08 +0530

    initial commit

---
+++ apps/angular-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-micro-front-end-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-app';
+}
